31faf6fec8e0445b2754258c1e972ab5d2361127
Daisuke Konishi <skydai1151@gmail.com>
2021-08-24 09:45:09 +0900

    handlerの型情報調整
    型情報調整に伴い発生していたTSのエラーに対応

--- handler.ts
+++ handler.ts
@@ -1,24 +1,33 @@
-const serverless = require("serverless-http");
-const express = require("express");
+import * as serverless from "serverless-http";
+import * as express from "express";
+import { APIGatewayEventRequestContext, APIGatewayProxyEvent, Context } from "aws-lambda";
 
 const app = express();
 
-app.get("/", (req, res, next) => {
+interface CustomRequest extends express.Request {
+  context: APIGatewayEventRequestContext
+}
+
+app.get("/", (req, res: express.Response) => {
   return res.status(200).json({
     message: "Hello from root!",
   });
 });
 
-app.get("/hello", (req, res, next) => {
+app.get("/hello", (req, res: express.Response) => {
   return res.status(200).json({
     message: "Hello from path!",
   });
 });
 
-app.use((req, res, next) => {
+app.use((req, res: express.Response) => {
   return res.status(404).json({
     error: "Not Found",
   });
 });
 
-module.exports.handler = serverless(app);
+export const handler = serverless(app, {
+  request: (req: CustomRequest, event: APIGatewayProxyEvent, _context: Context) => {
+    req.context = event.requestContext
+  }
+});
